2789ee3f3441628ca215929e7ad9b282b7ed3d1a
LinoKhan1 <linokhan1@gmail.com>
2023-02-14 15:25:22 +0200

    store and carts functionalities

--- store/views.py
+++ store/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.http import HttpResponse
 from .models import Product, Category
+from carts.views import CartItem, _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 # Create your views here.
@@ -14,11 +18,17 @@ def store(request, category_slug=None):
         product_count = products.count()
     else:
         # Query to fetch all products that are available
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        page_products = paginator.get_page(page)
+        product_count = products.count()
+
+
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': page_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
@@ -28,10 +38,23 @@ def product_detail(request, category_slug, product_slug):
 
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
-    return render(request, 'store/product_detail.html', context)
+    return render(request, 'store/product-detail.html', context)
+
+
+def search(request):
+   # if 'keyword' in request.GET:
+     #   keyword = request.GET['keyword']
+     #   if keyword:
+      #      products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q( product_name__icontains = keyword) )
+  #  context = {
+      #  'products': products,
+   # }
+    return render(request, 'store/store.html')
